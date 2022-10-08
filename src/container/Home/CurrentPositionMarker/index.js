42bd964610dfde3196316ce69409a74b380a3519
cyberjam <jamsince1994@naver.com>
2022-10-08 18:10:52 +0900

    🤡

--- src/container/Home/CurrentPositionMarker/index.js
+++ src/container/Home/CurrentPositionMarker/index.js
@@ -1,12 +1,7 @@
 import { React, useEffect, useState } from 'react';
 import Marker from '../../../components/NaverMap/Marker';
 
-function CurrentPositionMarker({
-    state,
-    onPositionRender,
-    locationId,
-    setLocationId,
-}) {
+function CurrentPositionMarker({ state, setState, locationId, setLocationId }) {
     // const [state, setState] = useState({
     //     center: {
     //         lat: 33.0,
@@ -16,41 +11,47 @@ function CurrentPositionMarker({
     //     isLoading: true,
     // });
 
-    const findMyLocation = () => {
-        if (navigator.geolocation) {
-            // GeoLocation을 이용해서 접속 위치를 얻어옵니다
-            const watchPositionId = navigator.geolocation.watchPosition(
-                (position) => {
-                    const center = {
-                        lat: position.coords.latitude, // 위도
-                        lng: position.coords.longitude, // 경도
-                    };
-                    onPositionRender({
-                        id: locationId,
-                        center,
-                        isLoading: false,
-                    });
-                    // setState((prev) => ({
-                    //     ...prev,
-                    //     id: locationId,
-                    //     center,
-                    //     isLoading: false,
-                    // }));
-                    // alert(state.center.lat);
-                },
-                (err) => {
-                    return 0;
-                },
-                { enableHighAccuracy: true, maximumAge: 10000, timeout: 5000 },
-            );
-            // alert(watchPositionId);
-            setLocationId(watchPositionId);
-        }
-    };
+    // const findMyLocation = () => {
+    //     if (navigator.geolocation) {
+    //         // GeoLocation을 이용해서 접속 위치를 얻어옵니다
+    //         const watchPositionId = navigator.geolocation.watchPosition(
+    //             (position) => {
+    //                 const center = {
+    //                     lat: position.coords.latitude, // 위도
+    //                     lng: position.coords.longitude, // 경도
+    //                 };
+    //                 setState((prev) => ({
+    //                     ...prev,
+    //                     id: locationId,
+    //                     center,
+    //                     isLoading: false,
+    //                 }));
+    //                 // alert(state.center.lat);
+    //             },
+    //             (err) => {
+    //                 setState((prev) => ({
+    //                     ...prev,
+    //                     errMsg: err.message,
+    //                     isLoading: false,
+    //                 }));
+    //             },
+    //             { enableHighAccuracy: true, maximumAge: 10000, timeout: 5000 },
+    //         );
+    //         // alert(watchPositionId);
+    //         setLocationId(watchPositionId);
+    //     } else {
+    //         // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
+    //         setState((prev) => ({
+    //             ...prev,
+    //             errMsg: 'geolocation을 사용할수 없어요..',
+    //             isLoading: false,
+    //         }));
+    //     }
+    // };
 
-    useEffect(() => {
-        findMyLocation();
-    }, []);
+    // useEffect(() => {
+    //     findMyLocation();
+    // }, []);
 
     return (
         <>
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import NaverMap from '../components/NaverMap';
 import Header from '../container/Home/Header';
 import HomeMarker from '../container/Home/HomeMarker';
 import { getSpreadSheetData } from '../libs/sheets';
-import { useMemo, useState } from 'react';
+import { useMemo, useState, useEffect } from 'react';
 import CurrentPositionMarker from '../container/Home/CurrentPositionMarker';
 
 export default function Home({ spreadSheetData }) {
@@ -26,9 +26,47 @@ export default function Home({ spreadSheetData }) {
     });
     const [locationId, setLocationId] = useState(0);
 
-    const handleCurrentPosition = (positionInfo) => {
-        setCurrentPositionState((prev) => ({ ...prev, ...positionInfo }));
+    const findMyLocation = () => {
+        if (navigator.geolocation) {
+            // GeoLocation을 이용해서 접속 위치를 얻어옵니다
+            const watchPositionId = navigator.geolocation.watchPosition(
+                (position) => {
+                    const center = {
+                        lat: position.coords.latitude, // 위도
+                        lng: position.coords.longitude, // 경도
+                    };
+                    setCurrentPositionState((prev) => ({
+                        ...prev,
+                        id: locationId,
+                        center,
+                        isLoading: false,
+                    }));
+                    // alert(state.center.lat);
+                },
+                (err) => {
+                    setCurrentPositionState((prev) => ({
+                        ...prev,
+                        errMsg: err.message,
+                        isLoading: false,
+                    }));
+                },
+                { enableHighAccuracy: true, maximumAge: 10000, timeout: 5000 },
+            );
+            // alert(watchPositionId);
+            setLocationId(watchPositionId);
+        } else {
+            // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
+            setCurrentPositionState((prev) => ({
+                ...prev,
+                errMsg: 'geolocation을 사용할수 없어요..',
+                isLoading: false,
+            }));
+        }
     };
+
+    useEffect(() => {
+        findMyLocation();
+    }, [locationId]);
 
     return (
         <div>
@@ -57,9 +95,6 @@ export default function Home({ spreadSheetData }) {
                     {currentPositionState.center.lat && (
                         <CurrentPositionMarker
                             state={currentPositionState}
-                            onPositionRender={handleCurrentPosition}
-                            locationId={locationId}
-                            setLocationId={setLocationId}
                         ></CurrentPositionMarker>
                     )}
 
